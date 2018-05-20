bce4a8cc067de16f31705f0ce2864bee86c33d5d
Justin Lettau <justin.b.lettau@gmail.com>
2018-05-20 17:01:23 -0400

    chore: update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -12,115 +12,115 @@ const uglify = require('gulp-uglify');
 
 // everything you need for development
 gulp.task('default', function (callback) {
-    runSequence(
-        'clean.dist',
-        'test.js',
-        'build.js',
-        'build.sass',
-        'inject',
-        callback
-    );
+  runSequence(
+    'clean.dist',
+    'test.js',
+    'build.js',
+    'build.sass',
+    'inject',
+    callback
+  );
 });
 
 // ready for publish
 gulp.task('release', function (callback) {
-    runSequence(
-        'clean.dist',
-        'build.js:release',
-        'build.sass:release',
-        'inject:release',
-        callback
-    );
+  runSequence(
+    'clean.dist',
+    'build.js:release',
+    'build.sass:release',
+    'inject:release',
+    callback
+  );
 });
 
 // Watch
 // --------------------
 
 gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['build.sass']);
+  gulp.watch('scss/**/*.scss', ['build.sass']);
 });
 
 // Clean
 // --------------------
 
 gulp.task('clean.dist', function (callback) {
-    return del('dist', callback);
+  return del('dist', callback);
 });
 
 // JavaScript
 // --------------------
 
 gulp.task('test.js', function () {
-    return gulp.src([
-        'js/**/*.js',
+  return gulp.src([
+    'js/**/*.js',
 
-        // exclude
-        '!js/analytics.js'
-    ])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+    // exclude
+    '!js/analytics.js'
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('build.js', function () {
-    return gulp.src([
-        'js/**/*.js',
+  return gulp.src([
+    'js/**/*.js',
 
-        // exclude
-        '!js/analytics.js'
-    ])
-        .pipe(gulp.dest('dist'));
+    // exclude
+    '!js/analytics.js'
+  ])
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build.js:release', function () {
-    return gulp.src('js/**/*.js')
-        .pipe(concat('app.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('js/**/*.js')
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 // HTML
 // --------------------
 
 gulp.task('inject', function () {
-    var sources = gulp.src('dist/**/*.js');
+  var sources = gulp.src('dist/**/*.js');
 
-    return gulp.src('*.html')
-        .pipe(inject(sources, { relative: true }))
-        .pipe(gulp.dest(''));
+  return gulp.src('*.html')
+    .pipe(inject(sources, { relative: true }))
+    .pipe(gulp.dest(''));
 });
 
 gulp.task('inject:release', function () {
-    var sources = gulp.src('dist/*.js');
+  var sources = gulp.src('dist/*.js');
 
-    return gulp.src('*.html')
-        .pipe(inject(sources, { relative: true }))
-        .pipe(gulp.dest(''));
+  return gulp.src('*.html')
+    .pipe(inject(sources, { relative: true }))
+    .pipe(gulp.dest(''));
 });
 
 // Fonts
 // --------------------
 
 gulp.task('fonts', function () {
-    return gulp.src([
-        'node_modules/font-awesome/fonts/fontawesome-webfont.*'
-    ])
-        .pipe(gulp.dest('fonts'));
+  return gulp.src([
+    'node_modules/font-awesome/fonts/fontawesome-webfont.*'
+  ])
+    .pipe(gulp.dest('fonts'));
 });
 
 // Sass
 // --------------------
 
 gulp.task('build.sass', function () {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build.sass:release', function () {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist'));
 });
--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    var elm = document.getElementById('tagline');
-    var index;
-    var lines = [
-        'Addicted to coffee.',
-        '01001000 01101001 00101110',
-        'jediMindTrick();'
-    ];
+  var elm = document.getElementById('tagline');
+  var index;
+  var lines = [
+    'Addicted to coffee.',
+    '01001000 01101001 00101110',
+    'jediMindTrick();'
+  ];
 
-    if (!elm) {
-        return;
-    }
+  if (!elm) {
+    return;
+  }
 
-    index = Math.floor(Math.random() * lines.length);
-    elm.innerText = lines[index];
+  index = Math.floor(Math.random() * lines.length);
+  elm.innerText = lines[index];
 })();
