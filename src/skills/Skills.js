f43e8a4e88eaf3bcbd1029d470bed441eb8409f6
Justin Lettau <justin.b.lettau@gmail.com>
2019-10-08 09:56:26 -0700

    chore: simplify skills section

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,24 +3,14 @@ import './Skills.scss';
 import React, { Component } from 'react';
 
 import angularPng from '../images/logos/angular.png';
-import bootstrapPng from '../images/logos/bootstrap.png';
 import csharpPng from '../images/logos/c-sharp.png';
 import cssPng from '../images/logos/css.png';
-import electronPng from '../images/logos/electron.png';
-import expressPng from '../images/logos/express.png';
-import gitPng from '../images/logos/git.png';
-import gulpPng from '../images/logos/gulp.png';
 import htmlPng from '../images/logos/html.png';
-import ionicPng from '../images/logos/ionic.png';
 import javascriptPng from '../images/logos/javascript.png';
-import mongodbPng from '../images/logos/mongodb.png';
 import nodejsPng from '../images/logos/nodejs.png';
-import npmPng from '../images/logos/npm.png';
-import pm2Png from '../images/logos/pm2.png';
 import reactPng from '../images/logos/react.png';
 import sassPng from '../images/logos/sass.png';
 import sqlPng from '../images/logos/sql.png';
-import threejsPng from '../images/logos/threejs.png';
 import typescriptPng from '../images/logos/typescript.png';
 
 export default class Skills extends Component {
@@ -54,16 +44,6 @@ export default class Skills extends Component {
       image: nodejsPng
     },
     {
-      name: 'Electron',
-      link: 'http://electron.atom.io/',
-      image: electronPng
-    },
-    {
-      name: 'Git',
-      link: 'https://git-scm.com/',
-      image: gitPng
-    },
-    {
       name: 'Sass',
       link: 'http://sass-lang.com/',
       image: sassPng
@@ -74,29 +54,9 @@ export default class Skills extends Component {
       image: cssPng
     },
     {
-      name: 'Gulp',
-      link: 'http://gulpjs.com/',
-      image: gulpPng
-    },
-    {
       name: 'HTML',
       link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
       image: htmlPng
-    },
-    {
-      name: 'Ionic',
-      link: 'http://ionicframework.com/',
-      image: ionicPng
-    },
-    {
-      name: 'Bootstrap',
-      link: 'http://getbootstrap.com/',
-      image: bootstrapPng
-    },
-    {
-      name: 'NPM',
-      link: 'https://www.npmjs.com/',
-      image: npmPng
     },
     {
       name: 'SQL',
@@ -107,26 +67,6 @@ export default class Skills extends Component {
       name: 'C#',
       link: 'https://en.wikipedia.org/wiki/C_Sharp_(programming_language)',
       image: csharpPng
-    },
-    {
-      name: 'MongoDB',
-      link: 'https://www.mongodb.com/',
-      image: mongodbPng
-    },
-    {
-      name: 'Express',
-      link: 'https://www.expressjs.com/',
-      image: expressPng
-    },
-    {
-      name: 'PM2',
-      link: 'http://pm2.keymetrics.io/',
-      image: pm2Png
-    },
-    {
-      name: 'Three.js',
-      link: 'https://threejs.org/',
-      image: threejsPng
     }
   ];
 
