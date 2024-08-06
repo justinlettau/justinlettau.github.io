b0470aabdedc2ff0a7f360d85464ef549b9dc7d2
Justin Lettau <justin.b.lettau@gmail.com>
2024-08-06 07:59:20 -0700

    chore: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://justinlettau.github.io',
   integrations: [tailwind(), partytown()],
